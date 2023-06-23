a32eaa8f5be3c3e0c68caabfcdd318b5f6ec9e85
SaintStaunch <118311369+SaintStaunch@users.noreply.github.com>
2023-06-23 12:40:31 +0000

    Add files via upload
    neo4j.sh

---
+++ roles/install-tools/files/neo4j.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+wget -O - https://debian.neo4j.com/neotechnology.gpg.key | sudo apt-key add -
+
+echo 'deb https://debian.neo4j.com stable 4.4' | sudo tee -a /etc/apt/sources.list.d/neo4j.list ;
+
+sudo apt update -y && sudo upgrade -y ;
+
+done ;
