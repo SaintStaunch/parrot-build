fde28e39ec95aac662c740d0e73c9ef8da490421
SaintStaunch <118311369+SaintStaunch@users.noreply.github.com>
2023-06-24 18:32:14 +1200

    Update neo4j.sh

--- roles/install-tools/files/neo4j.sh
+++ roles/install-tools/files/neo4j.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
-wget -O - https://debian.neo4j.com/neotechnology.gpg.key | sudo apt-key add -
+wget -O - https://debian.neo4j.com/neotechnology.gpg.key | sudo apt-key add - &&
 
-echo 'deb https://debian.neo4j.com stable 4.4' | sudo tee -a /etc/apt/sources.list.d/neo4j.list 
+echo 'deb https://debian.neo4j.com stable 4.4' | sudo tee -a /etc/apt/sources.list.d/neo4j.list &&
+
+sudo apt update -y && sudo apt install neo4j=1:4.4.20 -y &&
+
 
 done ;
